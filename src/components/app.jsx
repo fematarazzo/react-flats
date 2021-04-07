1987afc51114ea4947e0e6e7869f061445f016dd
Felipe Matarazzo <felipemps@protonmail.com>
2021-04-07 14:52:09 -0300

    googlemaps added with markers

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -1,7 +1,9 @@
 import React, { Component } from 'react';
+import GoogleMapReact from 'google-map-react';
 import flats from '../../data/flats';
 import FlatList from './flat_list';
-import GoogleMap from './googlemap_react';
+import Marker from './marker';
+
 
 class App extends Component {
   constructor(props) {
@@ -16,6 +18,13 @@ class App extends Component {
     this.setState({ selectedFlat: flats[index] });
   }
 
+  center() {
+    return {
+      lat: this.state.selectedFlat.lat,
+      lng: this.state.selectedFlat.lng
+    };
+  }
+
   render() {
     return (
       <div>
@@ -24,6 +33,11 @@ class App extends Component {
           selectedFlat={this.state.selectedFlat}
           selectFlat={this.selectFlat}
         />
+        <div className="map-container">
+          <GoogleMapReact defaultCenter={this.center()} defaultZoom={12}>
+            <Marker lat={this.state.selectedFlat.lat} lng={this.state.selectedFlat.lng} />
+          </GoogleMapReact>
+        </div>
       </div>
     );
   }
--- src/index.jsx
+++ src/index.jsx
@@ -3,7 +3,5 @@ import ReactDOM from 'react-dom';
 import App from './components/app';
 import '../assets/stylesheets/application.scss';
 
-const root = document.getElementById('root');
-if (root) {
-  ReactDOM.render(<App />, root);
-}
+
+ReactDOM.render(<App />, document.querySelector('#root'));
